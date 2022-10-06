52774d3cdebe18c098dddf7ee54be9ae1895e3d1
Gabriel C Silva <46769816+gbri-dev@users.noreply.github.com>
2022-10-06 13:00:11 -0300

    fundamento

---
+++ Arr.cs
@@ -0,0 +1,19 @@
+
+namespace Arr
+{
+
+    public class Arr
+    {
+        /*
+        var arr = new string[2];
+        arr[0] = "Item 1";
+
+        var arr2 = arr;
+
+        Console.WriteLine(arr[0]);
+        Console.WriteLine(arr2[0]);
+        */
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+            string nome = RetornaNome("Gabriel", "Campos");
+            Console.WriteLine(nome);
+
+            var mouse = new Product(01, "Mouse weibo gamer", 39.90, EProductType.Product);
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Nome);
+            Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.Type);
+            Console.WriteLine((int)mouse.Type);
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("C# Sonds goods!");
+        }
+
+        static string RetornaNome(
+            string nome,
+            string sobrenome,
+            //parametros opcionais são iniciados
+            double idade = 23,
+            string mes = " Junho"
+        )
+        {
+            return nome + " " + sobrenome + " idade " + idade.ToString() + " Anos." + mes;
+        }
+    }
+
+    struct Product
+    {
+        public Product(int id, string nome, double price, EProductType type)
+        {
+            Id = id;
+            Nome = nome;
+            Price = price;
+            Type = type;
+
+        }
+        public int Id;
+        public string Nome;
+
+        public double Price;
+
+        public EProductType Type;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+
+    enum EProductType
+    {
+        Product = 1,
+        Service = 2
+    }
+}
+
